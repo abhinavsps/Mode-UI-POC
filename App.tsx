d7581014b89c9ce86acf003288115dd93afca450
aditya-sps <152249472+aditya-sps@users.noreply.github.com>
2024-03-21 13:28:12 +0530

    Added dynamic languages

--- App.tsx
+++ App.tsx
@@ -6,29 +6,47 @@
  */
 
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {createContext, useState} from 'react';
 import {StatusBar, StyleSheet} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Navigation from './Navigation';
 import {isDarkMode} from './utils/functions';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import 'react-native-gesture-handler';
+import './services/i18next';
+import Languages from './components/Languages';
+import {useTranslation} from 'react-i18next';
+
+export const MyContext = createContext<any>({});
 
 function App(): React.JSX.Element {
+  const [showModal, setShowModal] = useState(false);
+  const {i18n} = useTranslation();
   let isdarkMode = isDarkMode();
   const backgroundStyle = {
     backgroundColor: isdarkMode ? Colors.darker : Colors.lighter,
   };
 
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <StatusBar
-          barStyle={isdarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
+    <MyContext.Provider value={{showModal, setShowModal}}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <StatusBar
+            barStyle={isdarkMode ? 'light-content' : 'dark-content'}
+            backgroundColor={backgroundStyle.backgroundColor}
+          />
+          <Navigation />
+        </NavigationContainer>
+        <Languages
+          modalVisible={showModal}
+          onItemPress={(value: string) => {
+            i18n.changeLanguage(value);
+            setShowModal(false);
+          }}
+          onClose={() => setShowModal(false)}
         />
-        <Navigation />
-      </NavigationContainer>
-    </SafeAreaProvider>
+      </SafeAreaProvider>
+    </MyContext.Provider>
   );
 }
 
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,4 +1,5 @@
 import {
+  Button,
   Image,
   ImageBackground,
   ScrollView,
@@ -8,15 +9,19 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {isDarkMode} from '../utils/functions';
 import {optionList, popularList, recommendedList} from '../utils/data';
+import {useTranslation} from 'react-i18next';
+import {MyContext} from '../App';
 
 const Home = () => {
   let isdarkMode = isDarkMode();
   const [value, setValue] = useState('');
   const [selected, setSelected] = useState('Location');
+  const {t} = useTranslation();
+  const {setShowModal} = useContext(MyContext);
 
   return (
     <SafeAreaView
@@ -24,147 +29,156 @@ const Home = () => {
         flex: 1,
         backgroundColor: isdarkMode ? 'rgba(0, 0, 0, 0.9)' : 'white',
       }}>
-      <ScrollView>
-        <Text style={{marginLeft: 20, color: isdarkMode ? 'white' : 'black'}}>
-          Explore{'\n'}
-          <Text style={{fontSize: 32, fontWeight: '500'}}>Aspen</Text>
-        </Text>
-        <TextInput
-          style={[styles.textInput, {color: isdarkMode ? 'black' : '#757575'}]}
-          value={value}
-          onChangeText={text => setValue(text)}
-          placeholder={'Find things to do'}
-          placeholderTextColor={'#B8B8B8'}
-        />
-        <View style={{marginTop: 30}}>
-          <ScrollView
-            horizontal
-            contentContainerStyle={{paddingHorizontal: 20}}
-            showsHorizontalScrollIndicator={false}>
-            {optionList.map((item, index) => (
-              <TouchableOpacity
-                style={{
-                  paddingHorizontal: 14,
-                  paddingVertical: 10,
-                  borderRadius: 18,
-                  backgroundColor:
-                    selected === item.title ? '#F3F8FE' : 'transparent',
-                }}
-                onPress={() => setSelected(item.title)}
-                key={index.toString()}>
-                <Text
+      <View style={{flex: 1}}>
+        <ScrollView>
+          <Text style={{marginLeft: 20, color: isdarkMode ? 'white' : 'black'}}>
+            {t('Explore')}
+            {'\n'}
+            <Text style={{fontSize: 32, fontWeight: '500'}}>{t('Aspen')}</Text>
+          </Text>
+          <TextInput
+            style={[
+              styles.textInput,
+              {color: isdarkMode ? 'black' : '#757575'},
+            ]}
+            value={value}
+            onChangeText={text => setValue(text)}
+            placeholder={t('placeholder')}
+            placeholderTextColor={'#B8B8B8'}
+          />
+          <View style={{marginTop: 30}}>
+            <ScrollView
+              horizontal
+              contentContainerStyle={{paddingHorizontal: 20}}
+              showsHorizontalScrollIndicator={false}>
+              {optionList.map((item, index) => (
+                <TouchableOpacity
                   style={{
-                    color: selected === item.title ? '#176FF2' : '#B8B8B8',
-                  }}>
-                  {item.title}
-                </Text>
-              </TouchableOpacity>
-            ))}
-          </ScrollView>
-        </View>
-        <View style={{marginTop: 40}}>
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              paddingHorizontal: 20,
-            }}>
+                    paddingHorizontal: 14,
+                    paddingVertical: 10,
+                    borderRadius: 18,
+                    backgroundColor:
+                      selected === item.title ? '#F3F8FE' : 'transparent',
+                  }}
+                  onPress={() => setSelected(item.title)}
+                  key={index.toString()}>
+                  <Text
+                    style={{
+                      color: selected === item.title ? '#176FF2' : '#B8B8B8',
+                    }}>
+                    {t(item.title)}
+                  </Text>
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+          </View>
+          <View style={{marginTop: 40}}>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                paddingHorizontal: 20,
+              }}>
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontWeight: '600',
+                  color: isdarkMode ? 'white' : 'black',
+                }}>
+                {t('Popular')}
+              </Text>
+              <Text style={{color: '#176FF2'}}>{t('See all')}</Text>
+            </View>
+            <ScrollView
+              horizontal
+              style={{marginTop: 20}}
+              contentContainerStyle={{paddingHorizontal: 10}}
+              showsHorizontalScrollIndicator={false}>
+              {popularList?.map((item, index) => (
+                <ImageBackground
+                  key={index.toString()}
+                  style={{
+                    height: 240,
+                    width: 188,
+                    marginHorizontal: 10,
+                    justifyContent: 'flex-end',
+                  }}
+                  borderRadius={20}
+                  source={{uri: item.url}}>
+                  <View
+                    style={[
+                      styles.popularTitleContainer,
+                      {backgroundColor: isdarkMode ? 'white' : '#4D5652'},
+                    ]}>
+                    <Text style={{color: isdarkMode ? 'black' : 'white'}}>
+                      {t(item.title)}
+                    </Text>
+                  </View>
+                  <View
+                    style={[
+                      styles.ratingContainer,
+                      {
+                        backgroundColor: isdarkMode ? 'white' : '#4D5652',
+                      },
+                    ]}>
+                    <Text style={{color: isdarkMode ? '#4D5652' : 'white'}}>
+                      {item.rating}
+                    </Text>
+                  </View>
+                </ImageBackground>
+              ))}
+            </ScrollView>
+          </View>
+          <View style={{marginTop: 40}}>
             <Text
               style={{
                 fontSize: 18,
                 fontWeight: '600',
+                marginLeft: 20,
                 color: isdarkMode ? 'white' : 'black',
               }}>
-              Popular
+              {t('Recommended')}
             </Text>
-            <Text style={{color: '#176FF2'}}>See all</Text>
+            <ScrollView
+              horizontal
+              style={{marginTop: 20}}
+              contentContainerStyle={{paddingHorizontal: 10}}
+              showsHorizontalScrollIndicator={false}>
+              {recommendedList?.map((item, index) => (
+                <View key={index.toString()} style={styles.recommendItem}>
+                  <Image
+                    source={{uri: item.url}}
+                    style={{width: 166, height: 102}}
+                    borderRadius={16}
+                  />
+                  <Text style={{fontSize: 16, fontWeight: '600', marginTop: 4}}>
+                    {t(item.title)}
+                  </Text>
+                  <Text style={{color: '#3A544F', marginLeft: 15}}>
+                    {t('Hot Deal')}
+                  </Text>
+                  <View
+                    style={[
+                      styles.durationView,
+                      {backgroundColor: isdarkMode ? 'white' : '#3A544F'},
+                    ]}>
+                    <Text
+                      style={{
+                        fontSize: 12,
+                        fontWeight: '600',
+                        color: isdarkMode ? 'black' : 'white',
+                      }}>
+                      {t(item.duration)}
+                    </Text>
+                  </View>
+                </View>
+              ))}
+            </ScrollView>
           </View>
-          <ScrollView
-            horizontal
-            style={{marginTop: 20}}
-            contentContainerStyle={{paddingHorizontal: 10}}
-            showsHorizontalScrollIndicator={false}>
-            {popularList?.map((item, index) => (
-              <ImageBackground
-                key={index.toString()}
-                style={{
-                  height: 240,
-                  width: 188,
-                  marginHorizontal: 10,
-                  justifyContent: 'flex-end',
-                }}
-                borderRadius={20}
-                source={{uri: item.url}}>
-                <View
-                  style={[
-                    styles.popularTitleContainer,
-                    {backgroundColor: isdarkMode ? 'white' : '#4D5652'},
-                  ]}>
-                  <Text style={{color: isdarkMode ? 'black' : 'white'}}>
-                    {item.title}
-                  </Text>
-                </View>
-                <View
-                  style={[
-                    styles.ratingContainer,
-                    {
-                      backgroundColor: isdarkMode ? 'white' : '#4D5652',
-                    },
-                  ]}>
-                  <Text style={{color: isdarkMode ? '#4D5652' : 'white'}}>
-                    {item.rating}
-                  </Text>
-                </View>
-              </ImageBackground>
-            ))}
-          </ScrollView>
-        </View>
-        <View style={{marginTop: 40}}>
-          <Text
-            style={{
-              fontSize: 18,
-              fontWeight: '600',
-              marginLeft: 20,
-              color: isdarkMode ? 'white' : 'black',
-            }}>
-            Recommended
-          </Text>
-          <ScrollView
-            horizontal
-            style={{marginTop: 20}}
-            contentContainerStyle={{paddingHorizontal: 10}}
-            showsHorizontalScrollIndicator={false}>
-            {recommendedList?.map((item, index) => (
-              <View key={index.toString()} style={styles.recommendItem}>
-                <Image
-                  source={{uri: item.url}}
-                  style={{width: 166, height: 102}}
-                  borderRadius={16}
-                />
-                <Text style={{fontSize: 16, fontWeight: '600', marginTop: 4}}>
-                  {item.title}
-                </Text>
-                <Text style={{color: '#3A544F', marginLeft: 15}}>Hot Deal</Text>
-                <View
-                  style={[
-                    styles.durationView,
-                    {backgroundColor: isdarkMode ? 'white' : '#3A544F'},
-                  ]}>
-                  <Text
-                    style={{
-                      fontSize: 12,
-                      fontWeight: '600',
-                      color: isdarkMode ? 'black' : 'white',
-                    }}>
-                    {item.duration}
-                  </Text>
-                </View>
-              </View>
-            ))}
-          </ScrollView>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </View>
+      <Button title={t('Change_Language')} onPress={() => setShowModal(true)} />
     </SafeAreaView>
   );
 };
